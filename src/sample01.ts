c2f9236fb7083c408e51e39d3cfe3541db8f7576
Jefferson Follmann <jeff.follmann@gmail.com>
2021-07-21 12:22:33 -0300

    Create branch to presentation

--- src/sample01.ts
+++ src/sample01.ts
@@ -3,29 +3,21 @@ import axios, { AxiosInstance } from 'axios';
 const githubUser = 'jfollmann';
 
 const callUserData = async (api: AxiosInstance) => {
-  const startTime = new Date();
+  // https://api.github.com/users/jfollmann
   const { data: { name, company, location } } = await api.get(`users/${githubUser}`);
-  const endTime = new Date();
-
-  const requestDuration = endTime.getTime() - startTime.getTime();
 
   console.log('User Data:');
   console.table({ name, company, location });
-  console.log(`Request duration (callUserData): ${requestDuration} ms.`);
 };
 
 const callUserFollowers = async (api: AxiosInstance) => {
-  const startTime = new Date();
+  // https://api.github.com/users/jfollmann/followers
   const { data } = await api.get<any[]>(`users/${githubUser}/followers`);
-  const endTime = new Date();
-
-  const requestDuration = endTime.getTime() - startTime.getTime();
 
   const table = data.map(({ id, login }) => ({ id, login }));
 
   console.log('User Followers:');
   console.table(table);
-  console.log(`Request duration (callUserFollowers): ${requestDuration} ms.`);
 };
 
 const run = async () => {
